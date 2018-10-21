2c9a14453bf19394840a16e052cbda185d3a8078
张永桥 <43776871+ZhangYongQiao@users.noreply.github.com>
2018-10-21 18:45:49 +0800

    10y21
    抽象

--- First.java
+++ First.java
@@ -1,42 +1,15 @@
 package aaa;
- 
-interface drawTest { //定义接口
-	public abstract void draw(); //此处abstract 可以省略，因为所有接口的方法都是抽象方法
-	//没有方法体  
-}
-public class First {
-	void function1() {
-		System.out.println("父类方法 能做些什么");
+
+public class First implements Jiekou{
+	int x;
+	int b;
+	public First(){
+		//do something
 	}
-	//内部类
-	public class A extends First implements drawTest{ //继承父类实现接口
-		public void draw() { //实现接口并对draw方法进行重写 使其有意义
-			System.out.println("A实现了接口能做些什么");
-		}
-		
-		void function1() {
-			System.out.println("A继承父类能做些什么");
-		}
-		
+	@Override
+	public void function() {
+		// TODO Auto-generated method stub
+		System.out.println("使用接口");
 	}
-	//内部类
-	public static class B extends First implements drawTest{//继承父类实现接口
-		public void draw() {
-			System.out.println("B实现了一个接口，能做些什么");
-		}
-		
-		void function1() {
-			System.out.println("B继承父类能做些什么");
-		}
-	}
-	
-	public static void main(String[] args) {
-		First aFirst = new First(); //关于内部类的调用
-		//非静态内部类的实例是寄生在外部类的实例中的，要访问非静态内部类，
-		//前提是外部类的实例已经存在。
-		drawTest[] aDrawTest= {aFirst.new A() ,new B()};//静态内部类与非静态内部类的调用区别
-		for(int i = 0; i<aDrawTest.length;i++) {
-		aDrawTest[i].draw();
-		}
-	}
+
 }
--- Second.java
+++ Second.java
@@ -1,8 +1,22 @@
 package aaa;
 
 public class Second extends First{
-	public  void function1() {
-		System.out.println("这是一个继承方法");
-	}
-	
+
+		int c;
+		
+		public static void main(String[] args) {
+			
+			Second aFirst = new Second();
+			aFirst.x=10;
+			aFirst.c=12;
+			First aFirst2 = new Second();
+			// First aFirst2;
+			//new Second();
+			//aFirst2 = new Second();
+			if(new Second() instanceof First) {
+				System.out.println("aaaaa");
+			}if(aFirst2 instanceof First) {
+				System.out.println("bbbbbb");
+			}
+		}
 }
